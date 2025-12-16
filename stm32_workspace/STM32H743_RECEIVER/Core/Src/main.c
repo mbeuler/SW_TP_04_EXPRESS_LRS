799ed595eb1ecb04787996bb4a7a750df84a1255
mbeuler <marcel.beuler@gmx.de>
2025-12-16 10:09:42 +0100

    DMA for USART3 (ELRS receiver) configured (with CubeMX)

--- stm32_workspace/STM32H743_RECEIVER/Core/Src/main.c
+++ stm32_workspace/STM32H743_RECEIVER/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -136,6 +137,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
