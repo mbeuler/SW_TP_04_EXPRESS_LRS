c136adf35284de9e4c3a3437e3ae33613322c3b6
mbeuler <marcel.beuler@gmx.de>
2026-02-07 16:31:46 +0100

    USBH HID: Add extern prototype for joystick init function. Declared USBH_HID_JoystickInit() in usbh_hid.c alongside mouse and keyboard init prototypes to enable joystick initialization.

--- stm32_workspace/STM32F407_TRANSMITTER/Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid.c
+++ stm32_workspace/STM32F407_TRANSMITTER/Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid.c
@@ -106,6 +106,7 @@ static void USBH_HID_ParseHIDDesc(HID_DescTypeDef *desc, uint8_t *buf);
 
 extern USBH_StatusTypeDef USBH_HID_MouseInit(USBH_HandleTypeDef *phost);
 extern USBH_StatusTypeDef USBH_HID_KeybdInit(USBH_HandleTypeDef *phost);
+extern USBH_StatusTypeDef USBH_HID_JoystickInit(USBH_HandleTypeDef *phost); // HID patch
 
 USBH_ClassTypeDef  HID_Class =
 {
